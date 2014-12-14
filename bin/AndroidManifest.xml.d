3965ea301ef6330bf8b4d1f31a37ec15e957d90d
toshihirock <toshihiro308@gmail.com>
2014-12-14 23:11:47 +0900

    delete files

--- bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/toshihirock/android-ndk-r10d/samples/hello-jni/bin/AndroidManifest.xml : \
-/Users/toshihirock/android-ndk-r10d/samples/hello-jni/AndroidManifest.xml \
--- bin/classes/R.java.d
+++
@@ -1,3 +0,0 @@
-/Users/toshihirock/android-ndk-r10d/samples/hello-jni/gen/com/example/hellojni/R.java \
- : /Users/toshihirock/android-ndk-r10d/samples/hello-jni/res/values/strings.xml \
-/Users/toshihirock/android-ndk-r10d/samples/hello-jni/bin/AndroidManifest.xml \
